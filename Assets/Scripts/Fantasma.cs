cd707eb05d0c4975987050803ea933cfb6f39c68
Xunkyart <xunky.art@gmail.com>
2026-04-22 23:35:40 +0200

    El fantasma existe y funciona

---
+++ Assets/Scripts/Fantasma.cs
@@ -0,0 +1,112 @@
+using UnityEngine;
+using System;
+using NUnit.Framework;
+using Unity.VisualScripting;
+
+public class Fantasma : MonoBehaviour
+{   string estado = "patrulla";
+
+    public float distanciaPatrulla = 3.0f;
+
+    public float velocidadPatrulla = 5.0f;
+
+    public float velocidadAtaque = 1.0f;
+
+    public float distanciaEvitar =3.0f;
+
+    public float distanciaAtaque = 3.0f;
+
+    bool dirPatruDrcha = true;
+
+    GameObject Personaje;
+
+    Vector3 posicionInicial;
+
+    Vector3 posicionLimitIzq, posicionLimitDrcha;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Personaje = GameObject.Find("Player");
+
+        posicionInicial = transform.position;
+
+        posicionLimitIzq = new Vector3(posicionInicial.x - distanciaPatrulla, posicionInicial.y, posicionInicial.z);
+
+        posicionLimitDrcha = new Vector3(posicionInicial.x + distanciaPatrulla, posicionInicial.y, posicionInicial.z);
+
+        estado = "patrulla";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(estado);
+        //PATRULLA
+        if(estado == "patrulla")
+        {
+            if(transform.position.x >= posicionLimitDrcha.x)
+            {
+                dirPatruDrcha = false;
+            }
+            else if (transform.position.x <= posicionLimitIzq.x)
+            {
+               dirPatruDrcha = true;
+            }
+
+            if (dirPatruDrcha == true)
+            {
+                transform.Translate(velocidadPatrulla*Time.deltaTime, 0, 0);
+                this.GetComponent<SpriteRenderer>().flipX = false;
+            }
+            else
+            {
+                transform.Translate(velocidadPatrulla*-1*Time.deltaTime,0,0);
+                this.GetComponent<SpriteRenderer>().flipX = true;
+            }
+        }
+
+        //REGRESO
+        //Si nos alejamos lo suficiente, el enemigo deja de perseguirnos y vuelve a su patrulla
+
+        float distanciaConPlayer = Vector3.Distance(transform.position, Personaje.transform.position);
+        if(distanciaConPlayer <= distanciaAtaque)
+        {
+            estado = "ataque";
+        }
+        if(distanciaConPlayer > distanciaEvitar&&estado=="ataque")
+        {
+            estado ="regreso";
+        } 
+
+         if (estado == "regreso")
+        {
+            transform.position = Vector3.MoveTowards(transform.position, posicionInicial, velocidadPatrulla*Time.deltaTime);
+            if (transform.position == posicionInicial)
+            {
+                estado="patrulla";
+            }
+        }   
+        //ATAQUE
+
+        if (estado == "ataque")
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Personaje.transform.position, velocidadAtaque * Time.deltaTime);
+        }
+   }
+   void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.name == "Player")
+        {
+            GameManager.vidas -=1;
+            Personaje.GetComponent<Movimiento>().Respawnear();
+        }
+
+        if(col.gameObject.name == "Fuego")
+        {
+            Destroy(col.gameObject, 0.5f);
+            Destroy(this.gameObject,0.5f);
+        }
+    }
+
+}
