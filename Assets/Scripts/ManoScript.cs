43be1cf1c29bc972c0456668a021f1531ff2511f
Xunkyart <xunky.art@gmail.com>
2026-04-28 17:48:38 +0200

    Victoria

--- Assets/Scripts/ManoScript.cs
+++ Assets/Scripts/ManoScript.cs
@@ -1,20 +1,51 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class ScriptUIVictoria : MonoBehaviour
 {
     public GameObject panelVictoria;
     public GameObject panelDerrota;
+    public GameObject panelUI;
+    GameObject player;
+    GameObject manoSprite;
+    GameObject fantasmas;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         panelVictoria.SetActive(false);
         panelDerrota.SetActive(false);
+        player = GameObject.FindWithTag("Player");
+        manoSprite = GameObject.Find("ManoSprite");
+        fantasmas = GameObject.Find("==PELIGRO==");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Trigger");
+        if(col.name == "Player")
+        {
+            manoSprite.GetComponent<Animator>().SetBool("Cerrar",true);
+            StartCoroutine(IniciarTemporizador());
+        }
+    }
+    IEnumerator IniciarTemporizador(){
+        yield return new WaitForSeconds(1.1f);
+        Destroy(fantasmas);
+        Destroy(player);
+        yield return new WaitForSeconds(1.2f);
+        panelUI.SetActive(false);
+        panelVictoria.SetActive(true);
+    }
+
+     public void VovlerInicio()
+    {
+        SceneManager.LoadScene("Inicio");
     }
 }
